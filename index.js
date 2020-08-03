4d472f36d5f5f403a2e7a447ba266b574ae7de77
apppleblue <47538583+apppleblue@users.noreply.github.com>
2020-08-03 14:49:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const bodyParser = require("body-parser");
-const func = require('./databaseFunctions');
-const funcFD = require('./faceDetection');
 const funcBFR = require('./basicFaceRecognition');
-
 
 var connections = [];
 
@@ -21,57 +18,6 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
-
-
-//Getting form data
-app.post('/addNew', function (req, res) {
-    console.log("Adding new user");
-    console.log("name:" + req.body.name);
-
-    const newUserobj = {name: req.body.name, age: req.body.age, uni: req.body.uni};
-
-    func.sendFormData('addNewUser', newUserobj);
-
- res.end();
-});
-
-// func.sendFormData('getTotalUsers', null);
-// func.sendFormData('getUserDetails', null);
-//
-// let r = func.testReturn();
-//
-// setTimeout(function () {
-//     console.log(r);
-// }, 3000);
-//
-
-//io.sockets.emit('dbOutput', r);
-
 funcBFR.runModel('lbph');
-
-// Working area end
-
-http.listen(8000, function(){
-    console.log('listening on *:8000');
-    io.on('connection', function (socket) {
-        connections.push(socket);
-        console.log('New User');
-
-        socket.on('image', funcFD.processImg);
-
-        // socket.on('image', function (b64) {
-        //     var htmlImg = funcFD.processImg(b64);
-        //
-        //     console.log(htmlImg);
-        //
-        //     //sendImage(htmlImg);
-        // });
-
-    });
-});
-
-module.exports = {
-    sendImage: function (htmlImg){
-        io.sockets.emit('outputImage', htmlImg);
-    }
-};
+funcBFR.runModel('fisher');
+funcBFR.runModel('eigen');
